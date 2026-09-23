bf5372775eef7fbae117d33774db6cab964c2efd
Fabrice Maguy <f.maguy@gmail.com>
2026-09-23 22:08:11 +0200

    security: replace innerHTML with textContent in CookieConsentBanner

--- web/src/components/legal/CookieConsentBanner.tsx
+++ web/src/components/legal/CookieConsentBanner.tsx
@@ -27,7 +27,7 @@ export default function CookieConsentBanner() {
       document.head.appendChild(script1);
 
       const script2 = document.createElement('script');
-      script2.innerHTML = `
+      script2.textContent = `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
